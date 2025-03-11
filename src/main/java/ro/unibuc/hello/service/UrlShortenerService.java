32f7222d174f82edb05659d3d921f709052b56d6
Dragos <dragosionut2003@yahoo.ro>
2025-03-11 22:20:02 +0200

    Fixed url generation

--- src/main/java/ro/unibuc/hello/service/UrlShortenerService.java
+++ src/main/java/ro/unibuc/hello/service/UrlShortenerService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import ro.unibuc.hello.data.ShortUrlEntity;
 import ro.unibuc.hello.data.ShortUrlRepository;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Optional;
 
 @Service
@@ -15,7 +17,8 @@ public class UrlShortenerService {
     private static final String Base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final int shortUrlLength = 6;
 
-    // private static final String baseSiteUrl = "http://localhost:8080/";
+    private static final SecureRandom Random = new SecureRandom();
+
 
     public String createShortUrl(String originalUrl){
         Optional<ShortUrlEntity> existingLink = Optional.ofNullable(shortUrlRepository.findByOriginalUrl(originalUrl));
@@ -24,8 +27,9 @@ public class UrlShortenerService {
         }
         ShortUrlEntity newShortUrl = new ShortUrlEntity();
         newShortUrl.setOriginalUrl(originalUrl);
+        newShortUrl = shortUrlRepository.save(newShortUrl);
 
-        String shortUrl = shortenUrl(newShortUrl.getId());
+        String shortUrl = shortenUrl();
 
         newShortUrl.setShortenedUrl(shortUrl);
 
@@ -40,12 +44,15 @@ public class UrlShortenerService {
                 .orElseThrow(() -> new RuntimeException("URL not found"));
     }
 
-    private String shortenUrl(Long Id){
+    private String shortenUrl(){
         StringBuilder shortUrl = new StringBuilder();
-        // shortUrl.append(baseSiteUrl);
-        for(int urlCharIndex = 0; urlCharIndex < shortUrlLength; urlCharIndex++){
-            shortUrl.append(Base62.charAt((int)(Id % 62)));
-            Id /= 62;
+        boolean shortUrlReady = false;
+        while(!shortUrlReady) {
+            for (int urlCharIndex = 0; urlCharIndex < shortUrlLength; urlCharIndex++) {
+                shortUrl.append(Base62.charAt(Random.nextInt(62)));
+            }
+            if(Optional.ofNullable(shortUrlRepository.findByShortenedUrl(shortUrl.toString())).isEmpty())
+                shortUrlReady = true;
         }
         return shortUrl.toString();
     }
