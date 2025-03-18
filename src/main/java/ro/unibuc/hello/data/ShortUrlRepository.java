f175b2cf3ced00a4c582b2777aacf12ee4afeed6
Dragos <dragosionut2003@yahoo.ro>
2025-03-18 14:44:23 +0200

    Added link expiration and stats controller

--- src/main/java/ro/unibuc/hello/data/ShortUrlRepository.java
+++ src/main/java/ro/unibuc/hello/data/ShortUrlRepository.java
@@ -3,9 +3,13 @@ package ro.unibuc.hello.data;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface ShortUrlRepository extends MongoRepository<ShortUrlEntity, String> {
     ShortUrlEntity findByOriginalUrl(String originalUrl);
 
     ShortUrlEntity findByShortenedUrl(String shortenedUrl);
+
+    void deleteByExpirationDateBefore(LocalDateTime expirationDate);
 }
