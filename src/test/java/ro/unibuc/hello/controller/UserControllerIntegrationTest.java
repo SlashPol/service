be6d576a6d4cc0ef84208c04859e12ae185f6ef7
MarcuDragosIonut <98226017+MarcuDragosIonut@users.noreply.github.com>
2025-04-09 09:30:03 +0000

    changed localhost to host.docker.internal

--- src/test/java/ro/unibuc/hello/controller/UserControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/hello/controller/UserControllerIntegrationTest.java
@@ -30,7 +30,7 @@ public class UserControllerIntegrationTest {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        final String MONGO_URL = "mongodb://localhost:";
+        final String MONGO_URL = "mongodb://host.docker.internal:";
         final String PORT = String.valueOf(mongoDBContainer.getMappedPort(27017));
 
         registry.add("mongodb.connection.url", () -> MONGO_URL + PORT);
